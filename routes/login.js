099622b8013c7266fc3f50d7e3cae7edd57fefb0
maehara08 <aso.riiiku08@gmail.com>
2016-07-14 12:28:00 +0900

    login get method

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send("This is login api!");
+router.get('/', function (req, res, next) {
+    res.send("This is login api!");
+});
+
+router.get('/signup', function (req, res, next) {
+    res.send("This is sign up page");
+});
+
+router.get('/signin', function (req, res, next) {
+    res.send("This is sign in page!!!")
+
 });
 
 module.exports = router;
