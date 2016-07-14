67e80178b5d70a8ef8c5dc10dee14af8ab626b9b
maehara08 <aso.riiiku08@gmail.com>
2016-07-14 11:38:02 +0900

    Initial commit

---
+++ app2.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+app.get('/sample',function (req,res) {
+  res.send("Hello world!");
+  console.log("get/")
+  
+});
+
+
+app.listen(process.env.PORT || 8080,function () {
+  console.log('Example app listening on port 8080!');
+});
