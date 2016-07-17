3d056aaf9c4b85e73c72526c4ca79820441a0309
maehara08 <aso.riiiku08@gmail.com>
2016-07-17 22:48:53 +0900

    maps js module

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var logins = require('./routes/login');
+var maps=require('./routes/maps');
 
 
 var app = express();
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/login',logins);
+app.use('maps',maps);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
--- routes/maps.js
+++ routes/maps.js
@@ -1,3 +1,8 @@
 /**
  * Created by riku_maehara on 2016/07/17.
  */
+
+var express=require('express');
+var router=express.Router();
+
+module.exports=router;
